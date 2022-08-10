926f42d56e1f5746c2dfe69fc0b036cc3fdee9bc
Thatgiove <47024965+Thatgiove@users.noreply.github.com>
2022-08-10 21:33:11 +0200

    Aggiunto DeckButton + shaders

---
+++ Assets/Scripts/Buttons/DeckButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckButton : MonoBehaviour
+{
+    Material mat;
+    float value = 0;
+    float valueFI = 0;
+    
+    float shineAmount = 0.09f;
+    float fishEyeAmount = 0.1f;
+
+    bool toRx;
+    bool toRxFI;
+
+    void Start()
+    {
+        mat = GetComponent<Image>().material;
+    }
+
+    void Update()
+    {
+        if (mat.GetFloat("_ShineLocation") <= 0) { toRx = true; }
+        else if (mat.GetFloat("_ShineLocation") >= 1) { toRx = false; }
+
+        if (mat.GetFloat("_FishEyeUvAmount") <= 0) { toRxFI = true; }
+        else if (mat.GetFloat("_FishEyeUvAmount") >= 0.14) { toRxFI = false; }
+
+        if (toRx)
+            value += shineAmount;
+        else
+            value -= shineAmount;
+
+        if (toRxFI)
+            valueFI += fishEyeAmount;
+        else
+            valueFI -= fishEyeAmount;
+
+        mat.SetFloat("_ShineLocation", value * Time.fixedDeltaTime);
+        mat.SetFloat("_FishEyeUvAmount", valueFI * Time.fixedDeltaTime);
+    }
+}
